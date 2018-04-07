320c6e763f6cdcb5b9ef30e90c8a8581c38de959
Lior Alon <alonlior@gmail.com>
2018-04-07 17:34:54 +0300

    fixed iterable methodes

--- Ex1.py
+++ Ex1.py
@@ -86,8 +86,19 @@ def distinct(input_path, column_index, output_path):
         input_file, input_file_extension = input_vars[0], input_vars[1]
 
     required_column = [x[column_index] for x in input_file]
+    is_iterable = False
+    if required_column[0] in ['[]\n', '()\n', '{}\n', '[]', '()', '{}']: is_iterable = True
+    else:
+        try:
+            x = eval(required_column[0])
+            try:
+                iter(x)
+            except TypeError:
+                is_iterable=True
+        except SyntaxError:
+            pass
 
-    if (is_int(required_column[0]) or is_str(required_column[0])):
+    if not is_iterable:
         sorted_set = sorted(set(required_column))
     else:
         unsorted_set = set(required_column)
@@ -114,7 +125,11 @@ def union(input1_path, input2_path, output_path):
         print('files extensions should be the same')
         return
 
-    if (is_file_structure_consistent(input1_path) == -1 or is_file_structure_consistent(input2_path) == -1):
+    if (is_file_structure_consistent(input1_path) == -1):
+        print(input1_path + 'structure is not consistent, different number of attributes')
+        return
+    if (is_file_structure_consistent(input2_path) == -1):
+        print(input2_path + 'structure is not consistent, different number of attributes')
         return
 
     file1 = read_file_by_lines(input1_path)
@@ -135,21 +150,48 @@ def union(input1_path, input2_path, output_path):
     if (file1_num_of_att != file2_num_of_att or (file1_first_line == None or file2_first_line == None)):
         print('Error! The table\'s format does not match')
         return
-    else:
-        if (file1_first_line != None and file2_first_line != None):
-            for att1, att2 in zip(file1_first_line, file2_first_line):
-                if ((is_int(att1) and not (is_int(att2))) or (is_int(att2) and not (is_int(att1)))): # can we except the types to be only int or string?
-                    print('Files structure is not consistent, different types')
-                    return
-                if ((is_str(att1) and not (is_str(att2))) or (is_str(att2) and not (is_str(att1)))):
-                    print('Files structure is not consistent, different types')
-                    return
+    elif check_both_lines_have_same_attr(file1_first_line,file2_first_line) == -1:
+        print('Error! The table\'s format does not match')
+        return
 
     file1_after_append = [line + '::' + file1_name for line in file1]
     file2_after_append = [line + '::' + file2_name for line in file2]
     whole_file_after_append = file1_after_append + file2_after_append
     write_file_replace_if_exists(output_path,whole_file_after_append)
 
+
+def check_both_lines_have_same_attr(line1,line2):
+    for att1_raw, att2_raw in zip(line1, line2):
+        att1 = att1_raw
+        att2 =att2_raw
+        one_is_empty_itr = att1 in ['[]\n','()\n','{}\n','[]','()','{}']
+        two_is_empty_itr = att2 in ['[]\n','()\n','{}\n','[]','()','{}']
+        if one_is_empty_itr or two_is_empty_itr:
+            if one_is_empty_itr and two_is_empty_itr: continue
+            if one_is_empty_itr:
+                att1 = att1[0] + '1,' + att1[1]
+            if two_is_empty_itr:
+                att2 = att2[0] + '1,' + att2[1]
+
+        one_is_str = False
+        two_is_str = False
+        try:
+            att1_as_code = eval(att1)
+        except SyntaxError:
+            one_is_str = True
+        try:
+            att2_as_code = eval(att2)
+        except SyntaxError:
+            two_is_str = True
+        if one_is_str or two_is_str:
+            if one_is_str == two_is_str:
+                continue
+            else:
+                return -1
+
+        if type(att1_as_code) is not type(att2_as_code):
+            return -1
+    return 0
 
 def seperate(input_path, output_path1, output_path2):
     if (not (os.path.exists(input_path))):
@@ -234,10 +276,11 @@ def is_file_structure_consistent(file_path):
                 if (len(line_splitted) != len(prev_line_splitted)):
                     print(file_path + ' structure is not consistent, different number of attributes')
                     return -1
-                for att1, att2 in zip(line_splitted, prev_line_splitted):
-                    if ((is_int(att1) and not (is_int(att2))) or (is_int(att2) and not (is_int(att1)))):
-                        print(file_path + ' structure is not consistent, different types')
-                        return -1
+                if check_both_lines_have_same_attr(line_splitted, prev_line_splitted) == -1:
+                    print(file_path + ' structure is not consistent, different number of attributes')
+                    return -1
+        return 0
+
 
 
 def write_file_replace_if_exists(file_path, file_content): #ata king
--- tests.py
+++ tests.py
@@ -91,14 +91,14 @@ class MyTestCase(unittest.TestCase):
     ##UNION Tests###
 
     def test_union_1_empty_2_null(self):
-        testargs = ['Ex1', 'UNION', r'blabla', r'files\emptyfile.txt', 'bla']
+        testargs = ['Ex1', 'UNION', r'blabla', r'files\extrafiles\emptyfile.txt', 'bla']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
         assert capturedOutput.buflist[0] == 'file 1 or file 2 does not exist'
 
     def test_union_1_empty_2_null(self):
-        testargs = ['Ex1', 'UNION', r'files\emptyfile.txt', r'blabla', 'bla']
+        testargs = ['Ex1', 'UNION', r'files\extrafiles\emptyfile.txt', r'blabla', 'bla']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
@@ -106,7 +106,7 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_union_1_null_2_empty(self):
-        testargs = ['Ex1', 'UNION', r'blabla', r'files\emptyfile.txt', 'bla']
+        testargs = ['Ex1', 'UNION', r'blabla', r'files\extrafiles\emptyfile.txt', 'bla']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
@@ -114,14 +114,14 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_union_1_empty_2_csv(self):
-        testargs = ['Ex1', 'UNION', r'files\noending', r'files\emptyfile.csv', 'bla']
+        testargs = ['Ex1', 'UNION', r'files\extrafiles\noending', r'files\extrafiles\emptyfile.csv', 'bla']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
         assert capturedOutput.buflist[0] == 'input files in the wrong formats'
 
     def test_union_1_txt_2_no(self):
-        testargs = ['Ex1', 'UNION', r'files\emptyfile.csv', r'files\noending', 'bla']
+        testargs = ['Ex1', 'UNION', r'files\extrafiles\emptyfile.csv', r'files\extrafiles\noending', 'bla']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
@@ -129,14 +129,14 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_union_1_txt_2_csv(self):
-        testargs = ['Ex1', 'UNION', r'files\emptyfile.txt', r'files\emptyfile.csv', 'bla']
+        testargs = ['Ex1', 'UNION', r'files\extrafiles\emptyfile.txt', r'files\extrafiles\emptyfile.csv', 'bla']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
         assert capturedOutput.buflist[0] == 'files extensions should be the same'
 
     def test_union_1_csv_2_txt(self):
-        testargs = ['Ex1', 'UNION', r'files\emptyfile.csv', r'files\emptyfile.txt', 'bla']
+        testargs = ['Ex1', 'UNION', r'files\extrafiles\emptyfile.csv', r'files\extrafiles\emptyfile.txt', 'bla']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
@@ -144,7 +144,7 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_union_1_empty_2_full(self):
-        testargs = ['Ex1', 'UNION', r'files\emptyfile.txt', r'files\items.txt', 'bla']
+        testargs = ['Ex1', 'UNION', r'files\extrafiles\emptyfile.txt', r'files\items.txt', 'bla']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
@@ -152,7 +152,7 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_union_1_full_2_empty(self):
-        testargs = ['Ex1', 'UNION', r'files\items.txt', r'files\emptyfile.txt', 'bla']
+        testargs = ['Ex1', 'UNION', r'files\items.txt', r'files\extrafiles\emptyfile.txt', 'bla']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
@@ -167,34 +167,64 @@ class MyTestCase(unittest.TestCase):
         assert capturedOutput.buflist[0] == 'Error! The table\'s format does not match'
 
     def test_union_bad_structure_1(self):
-        testargs = ['Ex1', 'UNION', r'files\users.txt', r'files\bad_structure.txt', 'bla.txt']
+        testargs = ['Ex1', 'UNION', r'files\users.txt', r'files\extrafiles\bad_structure.txt', 'bla.txt']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
-        assert capturedOutput.buflist[0] == 'files\\bad_structure.txt structure is not consistent, different number of attributes'
+        assert capturedOutput.buflist[0] == 'files\\extrafiles\\bad_structure.txt structure is not consistent, different number of attributes'
 
     def test_union_bad_structure_2(self):
-        testargs = ['Ex1', 'UNION', r'files\users.txt', r'files\bad_structure_2.txt', 'bla.txt']
+        testargs = ['Ex1', 'UNION', r'files\users.txt', r'files\extrafiles\bad_structure_2.txt', 'bla.txt']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
-        assert capturedOutput.buflist[0] == 'files\\bad_structure_2.txt structure is not consistent, different number of attributes'
+        assert capturedOutput.buflist[0] == 'files\\extrafiles\\bad_structure_2.txt structure is not consistent, different number of attributes'
 
     def test_union_bad_structure_3(self):
-        testargs = ['Ex1', 'UNION', r'files\users.txt', r'files\bad_structure_3.txt', 'bla.txt']
+        testargs = ['Ex1', 'UNION', r'files\users.txt', r'files\extrafiles\bad_structure_3.txt', 'bla.txt']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
-        assert capturedOutput.buflist[0] == 'files\\bad_structure_3.txt structure is not consistent, different number of attributes'
+        assert capturedOutput.buflist[0] == 'files\\extrafiles\\bad_structure_3.txt structure is not consistent, different number of attributes'
 
     def test_union_diffrent_types_str_int(self):
-        testargs = ['Ex1', 'UNION', r'files\users.txt', r'files\users (2).txt', 'bla.txt']
+        testargs = ['Ex1', 'UNION', r'files\extrafiles\intfile1.txt', r'files\extrafiles\strfile1.txt', 'bla.txt']
+        capturedOutput = StringIO()
+        sys.stdout = capturedOutput
+        parse_args(testargs)
+        assert capturedOutput.buflist[0] == 'Error! The table\'s format does not match'
+
+    def test_union_diffrent_types_str_iter(self):
+        testargs = ['Ex1', 'UNION', r'files\extrafiles\iterativefile1.txt', r'files\extrafiles\strfile1.txt', 'bla.txt']
+        capturedOutput = StringIO()
+        sys.stdout = capturedOutput
+        parse_args(testargs)
+        assert capturedOutput.buflist[0] == 'Error! The table\'s format does not match'
+
+
+    def test_union_diffrent_types_int_iter(self):
+        testargs = ['Ex1', 'UNION', r'files\extrafiles\intfile1.txt', r'files\extrafiles\strfile1.txt', 'bla.txt']
+        capturedOutput = StringIO()
+        sys.stdout = capturedOutput
+        parse_args(testargs)
+        assert capturedOutput.buflist[0] == 'Error! The table\'s format does not match'
+
+
+
+    def test_union(self):
+        testargs = ['Ex1', 'UNION', r'files\users.txt', r'files\users (2).txt', 'union1.txt']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
 
-    def test_union(self):
-        testargs = ['Ex1', 'UNION', r'files\users.txt', r'files\users (2).txt', 'bla.txt']
+    def test_union_2(self):
+        testargs = ['Ex1', 'UNION', r'files\items.txt', r'files\items (2).txt', 'union2.txt']
+        capturedOutput = StringIO()
+        sys.stdout = capturedOutput
+        parse_args(testargs)
+
+    def test_union_3(self):
+        testargs = ['Ex1', 'UNION', r'files\ratings.txt', r'files\ratings (2).txt', 'union3.txt']
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
         parse_args(testargs)
@@ -203,9 +233,12 @@ class MyTestCase(unittest.TestCase):
 
     ### END ###
 
+###SEPERATE TESTS###
 
 
 
+
+  ### END ###
 
 if __name__ == '__main__':
     unittest.main()
